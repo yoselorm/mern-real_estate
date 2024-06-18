948690c065d3761209d288210e31742214c37dc5
Selorm Ayornu <yoselorm@gmail.com>
2024-06-18 19:50:41 +0000

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+require('dotenv').config();
+
+
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('connected to db');
+
+}).catch((err) => {
+    console.log(err);
+
+})
 
 const PORT = 5000
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port  ${PORT}`);    
+app.listen(PORT, () => {
+    console.log(`Server is running on port  ${PORT}`);
 })
